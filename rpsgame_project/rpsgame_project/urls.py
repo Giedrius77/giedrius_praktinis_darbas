bbe7a46c3643f4a511a0632b6b99cb5f11e9b529
Giedrius77 <giedrius.zalionis@gmail.com>
2024-02-26 10:30:24 +0200

    completed and works

--- rpsgame_project/rpsgame_project/urls.py
+++ rpsgame_project/rpsgame_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('play/', include('rpsgame.urls')),
+    path('', RedirectView.as_view(url='play/', permanent=True)),  # Redirect root URL to 'play/' endpoint
 ]
