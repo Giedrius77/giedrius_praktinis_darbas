4a1630100d14f897fbd89f9dbf939b2d2ae2080c
Giedrius77 <giedrius.zalionis@gmail.com>
2024-02-28 11:48:43 +0200

    working with auto win/count

--- rpsgame_project/rpsgame/views.py
+++ rpsgame_project/rpsgame/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
 import random
@@ -9,13 +9,22 @@ def play_game(request):
         user_move = request.POST.get('move')
         computer_move = random.choice(moves)
 
+        # Automatic win and loss counting logic
+        win_count = request.session.get('win_count', 0)
+        loss_count = request.session.get('loss_count', 0)
+
         if user_move == computer_move:
             result = _("it's a tie!").capitalize    
         elif (user_move == _('rock') and computer_move == _('scissors')) or (user_move == _('paper') and computer_move == _('rock')) or (user_move == _('scissors') and computer_move == _('paper')):
             result = _("you win!").capitalize
+            win_count += 1
         else:
             result = _("computer wins!").capitalize
+            loss_count += 1
 
-        return render(request, 'rpsgame/play_game.html', {'result': result, 'user_move': user_move, 'computer_move': computer_move})
+        request.session['win_count'] = win_count
+        request.session['loss_count'] = loss_count
+
+        return render(request, 'rpsgame/play_game.html', {'result': result, 'user_move': user_move, 'computer_move': computer_move, 'win_count': win_count, 'loss_count': loss_count})
 
     return render(request, 'rpsgame/play_game.html')  
